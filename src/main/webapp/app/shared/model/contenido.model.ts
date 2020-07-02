8e4c10daed5103d4619cc322627d7e0fb54e3ef9
Carlos Madrid <dev.cmadrid@gmail.com>
2020-07-02 14:16:56 -0500

    UPDATE: Se actualiza modelo de entidad "contenido".

--- src/main/webapp/app/shared/model/contenido.model.ts
+++ src/main/webapp/app/shared/model/contenido.model.ts
@@ -1,8 +1,11 @@
 export interface IContenido {
   id?: number;
   contenido?: string;
+  nombre?: string;
+  extension?: string;
+  peso?: number;
 }
 
 export class Contenido implements IContenido {
-  constructor(public id?: number, public contenido?: string) {}
+  constructor(public id?: number, public contenido?: string, public nombre?: string, public extension?: string, public peso?: number) {}
 }
