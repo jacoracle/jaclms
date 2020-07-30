df6b96ec03940a26e8eb70a37845a92240fe7956
jcleon-e <jleon@grupoeducare.com>
2020-07-30 16:35:24 -0500

    se cambia estructura html para empezar sección de búsqueda de sesiones

--- src/main/webapp/app/home-learning/home-learning.module.ts
+++ src/main/webapp/app/home-learning/home-learning.module.ts
@@ -4,9 +4,10 @@ import { ConstructorSharedModule } from 'app/shared/shared.module';
 import { HomeLearningComponent } from 'app/home-learning/home-learning.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [ConstructorSharedModule, MatFormFieldModule, MatInputModule],
+  imports: [RouterModule, ConstructorSharedModule, MatFormFieldModule, MatInputModule],
   declarations: [HomeLearningComponent]
 })
 export class HomeLearningModule {}
