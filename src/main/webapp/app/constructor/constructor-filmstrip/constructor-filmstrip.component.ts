fb091430c2d3e7125a32930e916431229d2a526c
jcleon-e <jleon@grupoeducare.com>
2020-09-09 12:12:15 -0500

    se revierten cambios que subió poncho que contienen la bandera y los ajustes que hizo para intentar resolver el detalle del id undefined pero que no son necesarios porque ya se agrego el loader sin hacer uso de banderas ni modificar subscriptions, se encuentra en pruebas ese loader

--- src/main/webapp/app/constructor/constructor-filmstrip/constructor-filmstrip.component.ts
+++ src/main/webapp/app/constructor/constructor-filmstrip/constructor-filmstrip.component.ts
@@ -17,7 +17,6 @@ export class ConstructorFilmstripComponent implements OnInit, OnDestroy, AfterCo
   contentBlocks: any;
   subscription: Subscription;
   templates: ITipoBloqueComponentes[] = [];
-  contentBlocksComplete = false;
 
   constructor(
     private contentBlocksService: ContentBlocksService,
@@ -37,10 +36,6 @@ export class ConstructorFilmstripComponent implements OnInit, OnDestroy, AfterCo
     this.subscription = this.contentBlocksService.getSelectedBlockIndex().subscribe(selectedBlockIndex => {
       this.selectedContentBlockIndex = selectedBlockIndex;
     });
-    // Obtener contentBlocksComplete actualizados de filmStrip
-    this.subscription = this.contentBlocksService
-      .getContentBlocksComplete()
-      .subscribe(contentBlocksComplete => (this.contentBlocksComplete = contentBlocksComplete));
   }
 
   ngOnInit(): void {
@@ -81,7 +76,6 @@ export class ConstructorFilmstripComponent implements OnInit, OnDestroy, AfterCo
       res => {
         if (res.body) {
           this.contentBlocks = res.body;
-          this.contentBlocksService.setcontentBlocksComplete(false);
           this.contentBlocksService.setContentBlocks(this.contentBlocks);
         }
       },
