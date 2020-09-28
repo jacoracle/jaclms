78afdbebc90e71824a03e1ae988f4236a6250dc7
Luis Alfonso Ponce Gonzalez <lponce@grupoeducare.com>
2020-09-28 16:34:11 -0500

    Se agrega strong al listener para no cerrar editor

--- src/main/webapp/app/constructor-layout/constructor-layout.component.ts
+++ src/main/webapp/app/constructor-layout/constructor-layout.component.ts
@@ -44,6 +44,7 @@ export class ConstructorLayoutComponent implements OnInit, OnDestroy {
           e.path[0].tagName === 'LI' ||
           e.path[0].tagName === 'EM' ||
           e.path[0].tagName === 'U' ||
+          e.path[0].tagName === 'STRONG' ||
           e.path[0].outerHTML.indexOf('<div class="ql-editor ql-blank" data-gramm="false" contenteditable="true"') !== -1 ||
           e.path[0].outerHTML.indexOf('<div quill-editor-toolbar="" class="ql-toolbar') !== -1 ||
           e.path[0].outerHTML.indexOf('<div quill-editor-element="" class="ql-container ql-snow"></div>') !== -1 ||
